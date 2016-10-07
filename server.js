41668779f75145cbf55648db9bdfea92fa64946a
Hunny-14 <harvinder.dua12@gmail.com>
2016-10-08 01:50:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.sendFile('Article one will be displayed');
+});
+
+app.get('/article-two', function (req, res) {
+  res.sendFile('Article two will be displayed');
+});
+
+app.get('/article-three', function (req, res) {
+  res.sendFile('Article three will be displayed');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
