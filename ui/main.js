0b91b8b9abde50c422ae3baa5373d521723babe5
Hunny-14 <harvinder.dua12@gmail.com>
2016-10-10 19:58:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,11 +4,11 @@ console.log('Loaded!');
 //to move the image
 
 var img = document.getElementById('madi');
-var marginleft=0;
+var marginLeft=0;
 function moveright ()
 {
-    marginleft = marginleft + 10;
-    img.style.marginLeft = marginleft + 'px';
+    marginleft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
 }
 img.onlick = function(){
    var interval = setInterval(moveright, 100)
