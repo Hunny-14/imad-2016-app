e72ba503f77a37745191049fe45f5b2e2f562690
Hunny-14 <harvinder.dua12@gmail.com>
2016-10-10 19:32:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ var marginleft=0;
 function moveright()
 {
     marginleft=marginleft + 10;
-    pic.marginleft=marginleft+"px";
+    pic.style.marginleft = marginleft + "px";
 }
 pic.onlick=function(){
    var interval= setInterval(moverright,100)
