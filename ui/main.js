2aa92e53e51fa5fb4b89b88c77822af2bbfa2a46
Hunny-14 <harvinder.dua12@gmail.com>
2016-10-11 01:53:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,9 +26,10 @@ var button=document.getElementById("counter");
 button.onclick=function(){
     var request= new XMLHttpRequest();
    
-    request.onreadystatechange=function(){
-        if(request.readyState===XMLHttpRequest.DONE)
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE)
     {
+        alert("request made");
         if(request.status===200)
         {
             alert("request status is 200 ");
