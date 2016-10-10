5fa431888a51385522b4a087cc52130d644f70a7
Hunny-14 <harvinder.dua12@gmail.com>
2016-10-11 01:38:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ button.onclick=function(){
     var request= new XMLHttpRequest();
    
     request.onreadystatechange=function(){
-        if(request.onreadystatechange===XMLHttpRequest.DONE)
+        if(request.readyState===XMLHttpRequest.DONE)
     {
         if(request.status==200)
         {
