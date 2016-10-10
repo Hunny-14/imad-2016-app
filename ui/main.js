1698e8a69d3a2e2429428f64f33e08ed97dcf805
Hunny-14 <harvinder.dua12@gmail.com>
2016-10-10 19:08:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,13 @@
 console.log('Loaded!');
+
+
+//to move the image
+
+var pic=document.getElementById("img");
+pic.style.marginleft="100px";
+function moveright(){
+    
+}
+pic.onlick=function(){
+    setInterval(moverright,100)
+}
