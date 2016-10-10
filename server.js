ec9367087ccd4379c2207dd9709a4ce09bdc3dcf
Hunny-14 <harvinder.dua12@gmail.com>
2016-10-11 00:03:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,6 +70,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/counter',function(req,res){
+    counter+=1;
+    res.send(counter.toString());
+});
+
+
 app.get('/:urlstorag', function (req, res) {
     var articlename=req.params.urlstorage;
   res.send(createhtml(articles[articlename]));
@@ -88,12 +94,10 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
 var counter=0;
 
-app.get('/counter',function(req,res){
-    counter+=1;
-    res.send(counter.toString());
-});
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
